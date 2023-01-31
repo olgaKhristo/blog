a7e96d2aed7734fb9423cc35208588a5d4450a37
olgaKhristo <olga84h@gmail.com>
2023-01-31 20:04:25 +0000

    srat

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   home
+   <p>My name is {{name}} and my age is {{age}}</p>
+   <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  setup(){
+console.log('setup')
+let name = 'Mario'
+let age = 30
+const handleClick = () => {
+  console.log('you clicked me')
+}
+return{name: name, age: age, handleClick}
+  } 
 }
 </script>
