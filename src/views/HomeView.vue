a6a231f3d21677b80aa019ad634b3522884fb377
olgaKhristo <olga84h@gmail.com>
2023-02-05 00:24:57 +0000

    next

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="home">
    home
-   <p>My name is {{name}} and my age is {{age}}</p>
+   <p ref="p">My name is {{name}} and my age is {{age}}</p>
    <button @click="handleClick">click me</button>
+   <button @click="age ++">add 1 to age</button>
+   <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 
 
 export default {
   name: 'HomeView',
   setup(){
 console.log('setup')
-let name = 'Mario'
-let age = 30
+const p = ref(null)
+const name = ref( 'Mario')
+const age = ref(30)
+
 const handleClick = () => {
-  console.log('you clicked me')
+  name.value = 'Lue'
+  age.value = 35
+  console.log(p,p.value)
+ // p.value.classList.add('test')
+ // p.value.textContent = 'hi all'
 }
-return{name: name, age: age, handleClick}
+return{name: name, age: age, handleClick, p}
   } 
 }
 </script>
